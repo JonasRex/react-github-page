d498af4a683947e74cf980254a8b5d3a848ee577
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-04-19 17:27:05 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:JonasRex/JonasRex.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:JonasRex/react-github-pages.git main:gh-pages
+# git push -f git@github.com:JonasRex/react-github-page.git main:gh-pages
 
 cd -
